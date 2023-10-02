210225898602fde2752e167738eff65c60369a21
sainadhjaladi <110282195+sainadhjaladi@users.noreply.github.com>
2023-10-02 15:56:25 +0530

    Update 1772A.java

--- Codeforces/1772A.java
+++ Codeforces/1772A.java
@@ -3,15 +3,15 @@ public class MyClass {
     public static void main(String args[]) {
       Scanner sc = new Scanner(System.in);
       int t = sc.nextInt();
-       sc.nextLine();
       while(t-->0)
       {
-            String input = sc.nextLine();
-            String[] parts = input.split("\\+"); 
-            int a = Integer.parseInt(parts[0].trim()); 
-            int b = Integer.parseInt(parts[1].trim()); 
-            int res = a + b;
-            System.out.println(res);
+          String str = sc.next();
+          char ch1=str.charAt(0);
+          char ch2=str.charAt(2);
+            int a = Character.getNumericValue(ch1); 
+            int b = Character.getNumericValue(ch2); 
+          int res = a+b;
+          System.out.println(res);
       }
     }
 }
